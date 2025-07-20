97c8b7dd63552eb1cbe6a511073171e981a66b8d
Ahmadharmoush-1 <ahmadharmoush74@gmail.com>
2025-07-20 12:41:12 +0300

    added snipped code

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../components/ui/Button";
+import { Button } from "./ui/Button";
 import { ChevronDown } from "lucide-react";
 // import heroImage from "@/assets/hero-image.jpg";
 
